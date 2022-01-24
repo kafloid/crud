c1369f61215f47f490924e960563312d5ba16b20
kafloid <cafloivedu@outlook.com>
2022-01-23 22:39:34 -0500

    multiples modificaciones

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+//Pages
+import Login from './components/common/Login';
+import SignUp from './components/common/Register';
+import Home from './components/pages/Home';
+import Todo from './components/common/Todo';
 
 function App() {
   return (
-    <div>
 
-    </div>
+    <Routes>
+      <Route exact={true} path="/" element={<Home />}></Route>
+      <Route path="/login" element={<Login />}></Route>
+      <Route path="/register" element={<SignUp />}></Route>
+      <Route path="/todo" element={<Todo />}></Route>
+    </Routes>
+
   );
 }
 
